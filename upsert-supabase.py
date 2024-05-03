ad7801df1b7d0079264f4c5db1826ed2b19c31e2
jhicks2306 <jhicks2306@gmail.com>
2024-05-03 17:03:30 +0100

    Update API

--- upsert-supabase.py
+++ upsert-supabase.py
@@ -1,88 +1,71 @@
-from fastapi import FastAPI, File, UploadFile
+import requests
+import io
+import os
 from typing import List
-import chromadb
-from langchain_chroma import Chroma
+
+from fastapi import FastAPI, File, UploadFile
+from pydantic import BaseModel
+from supabase.client import Client, create_client
+
+from langchain_community.vectorstores import SupabaseVectorStore
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.embeddings import OllamaEmbeddings
 from langchain_text_splitters import CharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
-from dotenv import dotenv_values
-import os
 
 app = FastAPI()
 
+class PDFRequest(BaseModel):
+    name: str
+    user_ref: str
+    pdf_url: str
+
+def download_pdf(url):
+    # Make a GET request to download the PDF
+    response = requests.get(url)
+    print("Content-Type:", response.headers.get('Content-Type'))
+    response.raise_for_status()  # Raise an exception for bad responses
+    return response.content
+
 # Receive collection name and uploaded PDF, return doc_ids
 @app.post("/process_pdf/")
-# async def process_pdf(collection_name: str, pdf_file: UploadFile = File(...)):
-async def process_pdf(collection_name: str, pdf_file: str):
-    # # Save the uploaded PDF temporarily
-    # with open(pdf_file.filename, "wb") as buffer:
-    #     buffer.write(pdf_file.file.read())
+async def process_pdf(pdf_request: PDFRequest):
+
+    # GET pdf_data from Firebase storage
+    pdf_url = pdf_request.pdf_url
 
     # Load the document and split it into chunks
-    loader = PyPDFLoader(pdf_file)
+    loader = PyPDFLoader(pdf_url)
     documents = loader.load()
 
     # Split it into chunks
-    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    text_splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=100)
     docs = text_splitter.split_documents(documents)
+
+    for doc in docs:
+        doc.metadata.update({"user_ref": pdf_request.user_ref, "name": pdf_request.name})
 
     # Create the open-source embedding function
     # embedding_function = OllamaEmbeddings(model="mistral:instruct")
     embedding_function = OpenAIEmbeddings()
 
     # Connect to database
-    chroma_client = chromadb.PersistentClient(path="./mychroma")
+    supabase_url = os.environ.get("SUPABASE_URL")
+    supabase_key = os.environ.get("SUPABASE_SERVICE_KEY")
+    supabase: Client = create_client(supabase_url, supabase_key)
 
-    # Create collection
-    chroma_client.get_or_create_collection(name=collection_name)
+    # Access vectorstore
+    vector_store = SupabaseVectorStore(
+        client=supabase,
+        embedding=embedding_function,
+        table_name="documents",
+        query_name="match_documents",
+    )
 
-    # Connect to collection through langchain
-    lang_chroma = Chroma(client=chroma_client, collection_name=collection_name, embedding_function=embedding_function)
+    # Add documents to vectorstore
+    doc_ids = vector_store.add_documents(docs)
 
-    # Add documents to collection
-    doc_ids = lang_chroma.add_documents(docs)
-
-    return {"doc_ids": doc_ids}
-
-# Delete collection based on collection name
-@app.delete("/delete_collection/")
-async def delete_collection(collection_name: str):
-    # Connect to database
-    chroma_client = chromadb.PersistentClient(path="./mychroma")
-
-    # Check that collection exits
-    if collection_name not in [c.name for c in chroma_client.list_collections()]:
-        return {"message": f"Collection '{collection_name}' does not exist."}
-    else:
-        # Delete collection
-        chroma_client.delete_collection(collection_name)
-        return {"message": f"Collection '{collection_name}' deleted successfully."}
-
-# Receive collection name and uploaded PDF, return doc_ids
-@app.post("/query/")
-async def query(query: str):
-
-    # Create the open-source embedding function
-    embedding_function = OllamaEmbeddings(model="mistral:instruct")
-
-    # Connect to database
-    chroma_client = chromadb.PersistentClient(path="./mychroma")
-
-    # if collection_name not in [c.name for c in chroma_client.list_collections()]:
-    #     return {"message": f"Collection '{collection_name}' does not exist."}
-    
-    # # Create collection
-    # chroma_client.get_collection(name=collection_name)
-
-    # Connect to collection through langchain
-    lang_chroma = Chroma(client=chroma_client, embedding_function=embedding_function)
-
-    # Add documents to collection
-    docs = lang_chroma.similarity_search(query)
-
-    return {"docs": docs}
-
+    return {"ids": doc_ids}
 
 if __name__ == "__main__":
     import uvicorn
